536be211fb4ad2a92c004abfcf4cf17f907a8f17
Brendan Straughan <brendan.straughan@digital.hmrc.gov.uk>
2019-01-11 09:57:50 +0000

    SecondCommit

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -1,5 +1,14 @@
 package app
 
+import scala.io.StdIn
+
 object GreeterApplication extends App {
-    println("Hello")
-}
+
+        def greet(name: String) = println(s"Hello $name")
+
+        val name = StdIn.readLine("what is your name\n")
+
+    greet(name)
+
+    }
+
